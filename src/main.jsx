17db91ad920bfe3f3b8c288e64e1abd2a66f379f
Guilherme Moura <guidevmoura@gmail.com>
2026-01-20 15:08:47 -0300

    change package

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { HashRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <React.StrictMode>
     <App />
-  </HashRouter>
-)
+  </React.StrictMode>
+);
